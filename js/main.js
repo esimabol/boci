5b797a1486d2b9b222aeff74fc758db2744dd3b5
Eddy Simancas <edylber.simancas@outlook.com>
2023-01-29 20:30:54 -0500

    recuperando los pasos

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
 const startingPoint = () => {
-    
+
+    //Funcion dibujar linea
     const drawLine = (color,width, style, xi, yi, xf, yf) => {
-        lienzo.beginPath();
-        lienzo.strokeStyle =color;
-        lienzo.lineWidth = width;
-        lienzo.lineCap = style;
-        lienzo.moveTo(xi,yi);
-        lienzo.lineTo(xf,yf);
-        lienzo.stroke();
-        lienzo.closePath();
+        context.beginPath();
+        context.strokeStyle =color;
+        context.lineWidth = width;
+        context.lineCap = style;
+        context.moveTo(xi,yi);
+        context.lineTo(xf,yf);
+        context.stroke();
+        context.closePath();       
     }
- 
+
     const colorLinea = () => paletaColores.value;
 
     const grosorLinea = () => controlGrosor.value;
     
-
+// Borrador
     const borrador = ()=>{ 
+        let activar_borrador = document.querySelector('#eraser-icon-container');
         let borrador = document.querySelector('#colores');
+
+        if (borrar.checked == true) {
+            activar_borrador.style.border = '0.5px solid #00FFFF';  
+            activar_borrador.style.backgroundColor = 'white';
             borrador.value = "#ffffff";
-            return borrador;
+        } 
+        else {
+            activar_borrador.style.border = '1px solid #000000';  
+            activar_borrador.style.backgroundColor = 'transparent';
+        }
+           
     }
-    const limpiarDibujo = () => lienzo.clearRect(0, 0, 500, 500);
+    const limpiarDibujo = () => context.clearRect(0, 0, canvas.width, canvas.height);
 
     //EMPIEZA DIBUJO CON MOUSE
     const activarMouse = () => {        
@@ -29,29 +40,29 @@ const startingPoint = () => {
             activar_mouse.style.border = '0.5px solid #00FFFF';  
             activar_mouse.style.backgroundColor = 'white';      
         if (activarDibujoMouse.checked === true) {
-            canvasElement.addEventListener('mousedown', restricciones, false);
+            canvas.addEventListener('mousedown', restricciones, false);
         }
         else {
             activar_mouse.style.border = '1px solid #000000';
             activar_mouse.style.backgroundColor = 'transparent';
-            canvasElement.removeEventListener('mousedown', restricciones, false);
+            canvas.removeEventListener('mousedown', restricciones, false);
         }        
     }
-    const restricciones = (e)=>{  
+    const restricciones = (e)=>{                
         if(e.buttons === 1){
-            canvasElement.addEventListener('mousemove', dibujarConMouse, false);
-            canvasElement.addEventListener('mouseup', detenerDibujoConMouse, false);  
+            canvas.addEventListener('mousemove', dibujarConMouse, false);
+            canvas.addEventListener('mouseup', detenerDibujoConMouse, false);  
         }                
     } 
 
-    const dibujarConMouse = (e)=>{
-        let xpos = e.offsetX;
-        let ypos = e.offsetY;        
-        drawLine(colorLinea(), grosorLinea(), 'round', xpos - e.movementX, ypos - e.movementY, xpos, ypos);      
+    const dibujarConMouse = (e)=>{        
+         x = e.offsetX;
+         y = e.offsetY;             
+        drawLine(colorLinea(), grosorLinea(), 'round', x - e.movementX, y - e.movementY, x, y);      
     }
 
     const detenerDibujoConMouse = ()=>{
-        canvasElement.removeEventListener('mousemove', dibujarConMouse, false);
+        canvas.removeEventListener('mousemove', dibujarConMouse, false);
     }
 
     //EMPIEZA DIBUJO CON TECLADO
@@ -62,7 +73,7 @@ const startingPoint = () => {
              
         if(activarDibujoTeclas.checked === true){            
                 activarDibujoMouse.checked = false;
-                activarMouse();
+                // activarMouse();
                 document.addEventListener('keyup', dibujarConTeclado, false);
         }
         else {
@@ -72,9 +83,8 @@ const startingPoint = () => {
         }        
     }
     const dibujarConTeclado = (e) => {        
-        const desplazamiento = 90; 
+        const desplazamiento = 15;       
         
-
         switch(e.keyCode) {
             case teclas.up:
                 drawLine(colorLinea(), grosorLinea(), 'butt', x, y, x, y - desplazamiento);
@@ -102,49 +112,63 @@ const startingPoint = () => {
     const closeNav = () => {
         document.getElementById("tools").style.width = "0";
     } 
-      
+    const drawText = () => {
+        let firma = prompt('Ingrese su nombre: ')
+        context.font = '12px verdana, sans-serif';
+        context.textAlign= 'end';
+        context.fillText(firma, canvas.width, canvas.height);
+    }
+          
 
     //DECLARACIONES GENERALES
-    let canvasElement = document.getElementById('drawing_area');
-    let lienzo = canvasElement.getContext('2d');           
-    let ancho = canvasElement.width;
-    console.log(ancho);
-    let x =250;
-    let y =250;
+    let canvas = document.getElementById('drawing_area');
+        canvas.width = window.width;
+        canvas.height = window.height;
 
+    let context = canvas.getContext('2d');  
+ 
+    //Variables para guardar la posicion en el canvas
+    let x =0;
+    let y =0;
+   
     let teclas = {
         up:38,
         down:40,
         left:37,
         right:39
-    };    
+    };
+    
+    let signature_button = document.querySelector('#signature');
+        signature_button.addEventListener('click', drawText, false);    
     let activarDibujoTeclas = document.querySelector('#keyboard_selector');
         activarDibujoTeclas.addEventListener('change', activarTeclado, false);
+        activarDibujoTeclas.addEventListener('mousedown', activarTeclado, false);
     let activarDibujoMouse = document.querySelector('#mouse_selector');
         activarDibujoMouse.addEventListener('change', activarMouse, false);
-    // let borrar = document.querySelector('#borrador');
-    //     borrar.addEventListener('change', borrador, false);
+    let borrar = document.querySelector('#borrador');
+        borrar.addEventListener('change', borrador, false);
     let controlGrosor = document.querySelector('#grosor');
         controlGrosor.addEventListener('change', grosorLinea, false );
-    // let borrarDibujo = document.getElementById('borrarTodo');
-    //     borrarDibujo.addEventListener('click', limpiarDibujo, false);
+    let borrarDibujo = document.getElementById('borrarTodo');
+        borrarDibujo.addEventListener('click', limpiarDibujo, false);
     let open_btn = document.querySelector('#open');
         open_btn.addEventListener('click', openNav, false);
     let close_btn = document.querySelector('.closebtn');
         close_btn.addEventListener('click', closeNav, false);
     let paletaColores = document.querySelector('#colores');
     let colores = document.getElementsByClassName('box');
+    // let mouseDown = false;
     const colores_array = Array.from(colores);
         colores_array.forEach((pallete_color) => {
-            pallete_color.addEventListener('click',() => {
-               switch (pallete_color.id) {
+            pallete_color.addEventListener('click',() => {            
+            switch (pallete_color.id) {
                 case 'black':
                 paletaColores.value = '#000000';
                 break;
                 case 'gray':
                 paletaColores.value = '#736F6E';
                 break;
-                case 'darkred':
+                case 'darkred':                    
                 paletaColores.value = '#8B0000';
                 break;
                 case 'orange':
@@ -163,7 +187,7 @@ const startingPoint = () => {
                 paletaColores.value = '#4B0082';
                 break;
                 case 'purple':
-                paletaColores.value = '##800080';
+                paletaColores.value = '#800080';
                 break;
                 case 'white':
                 paletaColores.value = '#ffffff';
@@ -200,14 +224,13 @@ const startingPoint = () => {
                 break;
 
                 default:
-                paletaColores.value = '#000000';
+                paletaColores.value = '#000000';                
                 break;
-               }
+                }
             })
         });
    
-  
     
         
-}
-window.addEventListener('load', startingPoint, false);        
+} 
+window.addEventListener('load', startingPoint, false);
